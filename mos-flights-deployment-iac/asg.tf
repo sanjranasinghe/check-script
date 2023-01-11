a425cca1313fd0ed8ff3a86bdc6f829a918f5a6e
sanjaya_ranasinghe <sanjaya@moresandtechnologies.com>
2023-01-11 15:39:53 +0000

    terra

---
+++ mos-flights-deployment-iac/asg.tf
@@ -0,0 +1,29 @@
+resource "aws_launch_template" "foobar" {
+  name_prefix   = var.lunch_name
+  image_id      = "ami-0ecbbc82eb6075d47"
+  instance_type = "t2.micro"
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 20
+    }
+  }
+
+  monitoring {
+    enabled = true
+  }
+}
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["eu-west-2a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.foobar.id
+    version = "$Latest"
+  }
+}
---
+++ mos-flights-deployment-iac/variables.tf
@@ -0,0 +1,45 @@
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_subnet_count" {
+  type    = number
+  default = 2
+}
+
+variable "project" {
+  type    = string
+  default = "MOS-Flight"
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    project = "MOS-Flight"
+  }
+}
+
+variable "moresand_public_cidr_blocks" {
+  type    = list(string)
+  default = ["123.231.61.17"]
+}
+
+variable "mail_domain" {
+  type    = string
+  default = "example.example.com"
+}
+
+variable "region" {
+  type = string
+}
+
+#variable "ami_name" {
+#  type = string
+#}
+
+variable "lunch_name" {
+  type    = string
+  default = "flighapi"
+}
+
