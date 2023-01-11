f1df1fb298ce19b10269209a38ef59fa2aa83226
sanjaya ranasinghe <37567832+sanjranasinghe@users.noreply.github.com>
2023-01-11 21:28:41 +0530

    Update deploymnt.sh

--- deploymnt.sh
+++ deploymnt.sh
@@ -6,7 +6,7 @@ sed -i "/#the variable that I want to be incremented$/s/=.*#/=$next_n;#/" ${0}
 echo $next_n
 
 
-cd /root/ansible_flight
+#cd /root/ansible_flight
 ansible-playbook ec2.yml --extra-vars "script=sample"
 sleep 1m
 ansible-playbook ec2_ami.yml --extra-vars "version=$next_n"
