4afba3e99380aacc31d499547cb3c3bfb2278188
sanjaya ranasinghe <37567832+sanjranasinghe@users.noreply.github.com>
2023-01-11 21:12:29 +0530

    Update ami_replace.sh

--- ami_replace.sh
+++ ami_replace.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 
 a=`awk '{print $9}' ami_replace | sed 's/\"//g' | cut -b 1-21`
-i=`grep "image_id" /root/terr_flight/mos-flights-deployment-iac/asg.tf | cut -b 20-40`
+i=`grep "image_id" mos-flights-deployment-iac/asg.tf | cut -b 20-40`
 #echo "$i"
-sed -i "s/$i/$a/g" /root/terr_flight/mos-flights-deployment-iac/asg.tf
+sed -i "s/$i/$a/g" mos-flights-deployment-iac/asg.tf
 cd /root/terr_flight/mos-flights-deployment-iac/
 terraform apply
