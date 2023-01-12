c4e833247409768f4f1b663e2ad0d825b373741b
sanjaya ranasinghe <37567832+sanjranasinghe@users.noreply.github.com>
2023-01-12 19:36:19 +0530

    Update deploymnt.sh

--- deploymnt.sh
+++ deploymnt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-n=0;#the variable that I want to be incremented
+n=1;#the variable that I want to be incremented
 next_n=$[$n+1]
 sed -i "/#the variable that I want to be incremented$/s/=.*#/=$next_n;#/" ${0}
 echo $next_n
