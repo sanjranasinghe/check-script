de12dfe4cf1bccb71b34ee1dcbd0fd789ef6e021
sanjaya ranasinghe <37567832+sanjranasinghe@users.noreply.github.com>
2023-01-17 18:22:22 +0530

    Update instance_replace.sh

--- instance_replace.sh
+++ instance_replace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-a=`awk '{print $9}' instance_details | cut -b 2-20`
-i=`grep "instance_id" ec2_ami.yml | cut -b 22-40`
-sed -i "s/$i/$a/g" ec2_ami.yml
+a=`awk '{print $9}' /usr/local/src/flight-api/instance_details | cut -b 2-20`
+i=`grep "instance_id" /usr/local/src/flight-api/ec2_ami.yml | cut -b 22-40`
+sed -i "s/$i/$a/g" /usr/local/src/flight-api/ec2_ami.yml
