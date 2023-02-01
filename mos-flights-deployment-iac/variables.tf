dd4cf25a529b48153c1b3257bc20bdd8de49045f
sanjaya ranasinghe <37567832+sanjranasinghe@users.noreply.github.com>
2023-02-01 20:33:03 +0530

    Update variables.tf

--- mos-flights-deployment-iac/variables.tf
+++ mos-flights-deployment-iac/variables.tf
@@ -41,8 +41,8 @@ variable "mail_domain" {
 variable "ami_id_bandc_jcb" {
   type    = string
 }
-variable "ami_id_foobar" {
-  type    = string
-}
+#variable "ami_id_foobar" {
+#  type    = string
+#}
 
 
